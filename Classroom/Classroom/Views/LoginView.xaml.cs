c778a5b461f2fe3deaa8cc28cad3b79ef7ea283e
wuscier <1352288123@qq.com>
2017-12-24 22:13:48 +0800

    use textbox instead of passwordbox

--- Classroom/Classroom/Views/LoginView.xaml.cs
+++ Classroom/Classroom/Views/LoginView.xaml.cs
@@ -20,17 +20,68 @@ namespace Classroom.Views
     /// </summary>
     public partial class LoginView : Window
     {
+        private string _realPassword = string.Empty;
+        private string _fakePassword = string.Empty;
+
+        private string _lastEncodedPassword = string.Empty;
+
+        
+
+
         public LoginView()
         {
             InitializeComponent();
             DataContext = new LoginViewModel();
         }
 
-        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        private void password_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Key key = e.Key;
+            Console.WriteLine($"password：{password.Text}");
+            Console.WriteLine($"length：{password.Text.Length}");
 
-            Console.WriteLine(key);
+            if (password.Text != _lastEncodedPassword)
+            {
+                string realPasswordIncrement = password.Text.Substring(_lastEncodedPassword.Length);
+
+                _realPassword += realPasswordIncrement;
+                Console.WriteLine($"real password：{_realPassword}");
+
+                int length = password.Text.Length;
+                string encodedPassword = string.Empty;
+                for (int i = 0; i < length; i++)
+                {
+                    encodedPassword += "·";
+                }
+
+                _lastEncodedPassword = encodedPassword;
+                password.Text = encodedPassword;
+                password.SelectionStart = password.Text.Length;
+            }
         }
+
+        //private void password_PreviewKeyDown(object sender, KeyEventArgs e)
+        //{
+        //    Console.WriteLine(e.Key);
+        //}
+
+        //private void password_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        //{
+        //    Console.WriteLine($"input text：{e.Text}");
+
+        //    string realInputString = e.Text;
+        //    _realPassword += realInputString;
+
+        //    string fakeInputString = string.Empty;
+        //    for (int i = 0; i < realInputString.Length; i++)
+        //    {
+        //        fakeInputString += "*";
+        //    }
+
+        //    _fakePassword += fakeInputString;
+
+        //    password.Text = _fakePassword;
+
+        //    password.SelectionStart = password.Text.Length;
+        //}
     }
 }
