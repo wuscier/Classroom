5c1b1539a06616402f1c1cbf4bce6055c8f4cd5f
wuscier <1352288123@qq.com>
2017-12-24 11:26:14 +0800

    add ui_hook_dotnet_wrap class

---
+++ Classroom/zoom_sdk_dotnet_wrap_edu/ui_hook_dotnet_wrap.cpp
@@ -0,0 +1,62 @@
+#include "stdafx.h"
+#include "ui_hook_dotnet_wrap.h"
+//#include "../../zoom_sdk_c_sharp_wrap/zoom_sdk_dotnet_wrap_util.h"
+#include "../../zoom_sdk_c_sharp_wrap/h/ui_hook_interface.h"
+#include "../../zoom_sdk_c_sharp_wrap/wrap/sdk_wrap.h"
+
+namespace ZOOM_SDK_DOTNET_WRAP {
+
+	class UIHookControllerEventHanlder {
+	public:
+		static UIHookControllerEventHanlder& GetInst() {
+			static UIHookControllerEventHanlder inst;
+			return inst;
+		}
+
+		virtual void onUIActionNotify(ZOOM_SDK_NAMESPACE::UIHOOKHWNDTYPE type, MSG msg) {
+			if (CUIHookControllerDotNetWrap::Instance)
+			{
+				CUIHookControllerDotNetWrap::Instance->procUIActionNotify((UIHOOKHWNDTYPE)type, msg.message);
+			}
+		}
+
+	private:
+		UIHookControllerEventHanlder() {}
+	};
+
+	SDKError CUIHookControllerDotNetWrap::MonitorWndMessage(unsigned int wndMsgId, bool bAdd)
+	{
+		SDKError err = (SDKError)ZOOM_SDK_NAMESPACE::CSDKExtWrap::GetInst().GetUIHookerWrap().MonitorWndMessage(wndMsgId, bAdd);
+		return err;
+	}
+
+	SDKError CUIHookControllerDotNetWrap::MonitorWnd(String ^ className, bool bAdd)
+	{
+		return SDKError::SDKERR_SUCCESS;
+		//SDKError err = (SDKError)ZOOM_SDK_NAMESPACE::CSDKExtWrap::GetInst().GetUIHookerWrap().MonitorWnd(const_cast<wchar_t*>(PlatformString2WChar(className)), bAdd);
+		//return err;
+	}
+
+	SDKError CUIHookControllerDotNetWrap::Start()
+	{
+		SDKError err = (SDKError)ZOOM_SDK_NAMESPACE::CSDKExtWrap::GetInst().GetUIHookerWrap().Start();
+		return err;
+	}
+
+	SDKError CUIHookControllerDotNetWrap::Stop()
+	{
+		SDKError err = (SDKError)ZOOM_SDK_NAMESPACE::CSDKExtWrap::GetInst().GetUIHookerWrap().Stop();
+		return err;
+	}
+
+	void CUIHookControllerDotNetWrap::BindEvent()
+	{
+		ZOOM_SDK_NAMESPACE::CSDKExtWrap::GetInst().GetUIHookerWrap().m_cbonUIActionNotify = std::bind(&UIHookControllerEventHanlder::onUIActionNotify,
+			&UIHookControllerEventHanlder::GetInst(), std::placeholders::_1, std::placeholders::_2);
+	}
+
+	void CUIHookControllerDotNetWrap::procUIActionNotify(UIHOOKHWNDTYPE type, unsigned int msg)
+	{
+		event_onUIActionNotify(type, msg);
+	}
+}
