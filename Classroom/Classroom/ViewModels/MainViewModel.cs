55827c8c2585ea3e1a3f6736ccee4cfa7f896b82
wuscier <1352288123@qq.com>
2017-12-26 18:32:11 +0800

    register callbacks.

--- Classroom/Classroom/ViewModels/MainViewModel.cs
+++ Classroom/Classroom/ViewModels/MainViewModel.cs
@@ -12,7 +12,11 @@ namespace Classroom.ViewModels
         public MainViewModel()
         {
             SubscribeEvents();
+
+            RegisterCallbacks();
+
             IsMainCardSelected = true;
+
         }
 
         private bool _isMainCardSelected;
@@ -54,6 +58,8 @@ namespace Classroom.ViewModels
 
             _startClassToken = EventAggregatorManager.Instance.EventAggregator.GetEvent<StartClassEvent>().Subscribe((argument) =>
             {
+                CustomMeetingUI();
+
                 StartParam startParam = new StartParam()
                 {
                     apiuserStart = new StartParam4APIUser()
@@ -86,6 +92,47 @@ namespace Classroom.ViewModels
         private void UnsubscribeEvents()
         {
             EventAggregatorManager.Instance.EventAggregator.GetEvent<CardSelectedEvent>().Unsubscribe(_cardSelectedToken);
+        }
+
+
+        private void RegisterCallbacks()
+        {
+            IMeetingServiceDotNetWrap meetingServiceDotNetWrap = CZoomSDKeDotNetWrap.Instance.GetMeetingServiceWrap();
+
+            IMeetingParticipantsControllerDotNetWrap meetingParticipantsController = meetingServiceDotNetWrap.GetMeetingParticipantsController();
+
+            meetingServiceDotNetWrap.Add_CB_onMeetingStatusChanged((status,result)=>
+            {
+
+            });
+            meetingParticipantsController.Add_CB_onHostChangeNotification((userId) =>
+            {
+
+            });
+            meetingParticipantsController.Add_CB_onLowOrRaiseHandStatusChanged((bLow, userId)=>
+            {
+
+            });
+            meetingParticipantsController.Add_CB_onUserJoin((userIds)=>
+            {
+            });
+            meetingParticipantsController.Add_CB_onUserLeft((userIds)=>
+            {
+
+            });
+            meetingParticipantsController.Add_CB_onUserNameChanged((userId,userName)=>
+            {
+
+            });
+
+        }
+
+        private void CustomMeetingUI()
+        {
+            CMeetingConfigurationDotNetWrap.Instance.HideMeetingInfoFromMeetingUITitle(true);
+            CMeetingConfigurationDotNetWrap.Instance.SetBottomFloatToolbarWndVisibility(false);
+            CMeetingConfigurationDotNetWrap.Instance.EnableEnterAndExitFullScreenButtonOnMeetingUI(false);
+            CMeetingConfigurationDotNetWrap.Instance.EnableLButtonDBClick4SwitchFullScreenMode(false);
         }
     }
 }
