cc08e22f7e64107f9442b3eb2e4fcb7d480af822
wuscier <1352288123@qq.com>
2018-01-13 18:34:14 +0800

    modeling stroke

---
+++ Classroom/Classroom2/Models/StrokeModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Classroom.Models
+{
+    public class StrokeModel
+    {
+        public string ColorString { get; set; }
+        public byte Width { get; set; }
+        public byte Height { get; set; }
+
+        public List<PointModel> Points { get; set; }
+    }
+
+    public class PointModel
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
